cb9889d404a931638e77ab20eef083a5feae2626
Devin Gaffney <itsme@devingaffney.com>
2023-07-31 12:48:21 -0700

    CV2-3551 decouple batch size for sqs pulling, add env getter into helper

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,12 +1,18 @@
 import os
 from typing import Any
 import importlib
-def get_setting(current_value: Any, default_os_key: str):
+def get_environment_setting(os_key: str) -> str:
+    """
+    Get environment variable helper. Could be augmented with credential store if/when necessary.
+    """
+    return os.environ.get(os_key)
+
+def get_setting(current_value: Any, default_os_key: str) -> Any:
     """
     Get settings either via current passed value or backup
     default value accessed via os environment, defined by default_os_key.
     """
-    return current_value or os.environ.get(default_os_key)
+    return current_value or get_environment_setting(default_os_key)
 
 def get_class(prefix: str, class_name: str) -> Any:
     """
