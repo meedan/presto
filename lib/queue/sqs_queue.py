41d0cd0277fa9b898e455013822573c5657bfd5a
Devin Gaffney <itsme@devingaffney.com>
2023-07-31 13:13:26 -0700

    just move constant to top like all others

--- lib/queue/sqs_queue.py
+++ lib/queue/sqs_queue.py
@@ -4,8 +4,8 @@ import boto3
 from lib.queue.queue import Queue
 from lib.helpers import get_environment_setting
 
+MAX_MESSAGE_DEPTH = 10  # SQS allows up to 10 messages per batch
 class SQSQueue(Queue):
-    MAX_MESSAGE_DEPTH = 10  # SQS allows up to 10 messages per batch
     def __init__(self, input_queue_name: str, output_queue_name: str, batch_size: int):
         """
         Initialize SQS queue - requires string names for intput / output queues,
