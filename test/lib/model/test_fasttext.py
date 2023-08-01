716a4b6543cce6271cbec31b3440aa1fb83ea3a9
Amy Dunphy <43973948+amydunphy@users.noreply.github.com>
2023-08-01 15:39:18 -0700

    unittests for fasttext

---
+++ test/lib/model/test_fasttext.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from unittest.mock import MagicMock
+
+import numpy as np
+
+from lib.model.fasttext import FasttextModel
+
+class TestFasttextModel(unittest.TestCase):
+    def setUp(self):
+        self.model = FasttextModel()
+        self.mock_model = MagicMock()
+
+    def test_respond(self):
+        query = [{"body": {"text": "Hello, how are you?"}}, {"body": {"text": "我会说中文"}}]
+
+        response = self.model.respond(query)
+      
+        self.assertEqual(len(response), 2)
+        self.assertEqual(response[0], "__label__eng_Latn")
+        self.assertEqual(response[1], "__label__yue_Hant")
+
+if __name__ == '__main__':
+    unittest.main()
