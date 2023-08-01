0b376c2bbf92bbc7845d1039fc9eebe16564a557
Amy Dunphy <43973948+amydunphy@users.noreply.github.com>
2023-08-01 15:30:21 -0700

    update not sentencetransformer

--- lib/model/fasttext.py
+++ lib/model/fasttext.py
@@ -1,12 +1,31 @@
-from sentence_transformers import SentenceTransformer
+from typing import Union, Dict, List
+from lib.model.model import Model
 
-from lib.model.generic_transformer import GenericTransformerModel
-MODEL_NAME = 'facebook/fasttext-language-identification'
+import fasttext
+from huggingface_hub import hf_hub_download
 
-class Model(GenericTransformerModel):
-    BATCH_SIZE = 100
+
+class FasttextModel(Model):
     def __init__(self):
         """
-        Init Facebook FastText language identification model. Fairly standard for all vectorizers.
+        Load fasttext model
         """
-        super().__init__(MODEL_NAME)
+        model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
+        self.model = fasttext.load_model(model_path)
+
+    
+    def respond(self, docs: Union[List[Dict[str, str]], Dict[str, str]]) -> List[Dict[str, str]]:
+        """
+        Force messages as list of messages in case we get a singular item. Then, run fingerprint routine.
+        Respond can probably be genericized across all models.
+        """
+        if not isinstance(docs, list):
+            docs = [docs]
+        detectable_texts = [e.get("body", {}).get("text") for e in docs]
+        detected_langs = []
+        for text in detectable_texts:
+            detected_langs.append(self.model.predict(text)[0][0])
+
+        for doc, vector in zip(docs, detected_langs):
+            doc["response"] = detected_lang
+        return docs
