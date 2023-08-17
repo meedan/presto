e22ddaf643f009dda9ad07cfc5db97c4cb383831
Devin Gaffney <itsme@devingaffney.com>
2023-08-17 06:15:20 -0700

    DEVOPS-471 Add host

--- start_healthcheck_and_model_engine.sh
+++ start_healthcheck_and_model_engine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the first process in the background
-uvicorn main:app --reload &
+uvicorn main:app --host 0.0.0.0 --reload &
 
 # Start the second process in the foreground
 # This will ensure the script won't exit until this process does
