f25019acf373812d53532e1d4bea792247b04a95
Devin Gaffney <itsme@devingaffney.com>
2024-03-05 13:36:09 -0800

    reset args as dict

--- test/lib/model/test_model.py
+++ test/lib/model/test_model.py
@@ -6,7 +6,7 @@
 # class TestModel(unittest.TestCase):
 #     def test_respond(self):
 #         model = Model()
-#         self.assertEqual(model.respond(schemas.parse_message(body=schemas.GenericItem(id='123', callback_url="http://example.com/callback", text="hello"))), model.respond(schemas.parse_message(body=schemas.GenericItem(id='123', callback_url="http://example.com/callback", text="hello"), response=[])))
+#         self.assertEqual(model.respond(schemas.parse_message({"body": schemas.GenericItem(id='123', callback_url="http://example.com/callback", text="hello")})), model.respond(schemas.parse_message({"body": schemas.GenericItem(id='123', callback_url="http://example.com/callback", text="hello"), "response": []})))
 #
 # if __name__ == '__main__':
 #     unittest.main()
