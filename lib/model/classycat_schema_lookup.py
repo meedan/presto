21f713e9e4001dae42936ff02d3363792805ca50
ashkankzme <ashkankzme@gmail.com>
2024-06-27 13:53:26 -0700

    WIP: classy cat schema look up code done, ready for testing

--- lib/model/classycat_schema_lookup.py
+++ lib/model/classycat_schema_lookup.py
@@ -2,7 +2,8 @@ import os
 import json
 from lib.logger import logger
 from lib.model.model import Model
-from lib.s3 import load_file_from_s3
+from lib.s3 import load_file_from_s3, file_exists_in_s3
+from lib.schemas import GenericItem
 
 
 class Model(Model):
@@ -13,24 +14,33 @@ class Model(Model):
         self.schemas_bucket = os.getenv("CLASSYCAT_OUTPUT_BUCKET")
 
 
+    def schema_name_exists(self, schema_name):
+        return file_exists_in_s3(self.output_bucket, f"{schema_name}.json")
+
+
     def look_up_schema_id_by_name(self, schema_name):
+        object = load_file_from_s3(self.schemas_bucket, f"{schema_name}.json")
+        contents = json.loads(object)
+        return contents["schema_id"]
+
+
+    def process(self, schema_lookup_event: GenericItem) -> GenericItem:
+        # check if schema with the name exists, and if so return the id
+        schema_name = schema_lookup_event.parameters["schema_name"]
+
+        result = GenericItem()
+
+        if not self.schema_name_exists(schema_name):
+            result.text = f"Schema name {schema_name} does not exist"
+            return result
+
+        logger.debug(f"located schema_name record for '{schema_name}'")
+
         try:
-            object = load_file_from_s3(self.schemas_bucket, f"{schema_name}.json")
-            contents = json.loads(object)
-            return contents["schema_id"]
+            result.id = self.look_up_schema_id_by_name(schema_name)
+            result.text = "success"
+            return result
         except Exception as e:
             logger.error(f"Error looking up schema name {schema_name}: {e}")
-            return None
-
-
-    def process(self):
-        # check if schema with the name exists, and if so return the id
-        schema_name = event["schema_name"]
-        if not schema_name_exists(schema_name, s3_client, output_bucket):
-            return {
-                "statusCode": 404,  # TODO: somehow these are not getting turned into http status codes
-                "body": {"message": f"Schema name {schema_name} does not exist"},
-            }
-        logger.debug(f"located schema_name record for '{schema_name}'")
-        schema_id = get_schema_id_from_name(schema_name, s3_client, output_bucket)
-        return {"statusCode": 200, "body": {"schema_id": schema_id}}
+            result.text = f"Error looking up schema name {schema_name}: {e}"
+            return result
