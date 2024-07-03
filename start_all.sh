a53d4d3a5ebaa5248eeee06713915bff4850c1b3
Devin Gaffney <itsme@devingaffney.com>
2024-07-03 06:36:36 -0700

    Add all tasks to compose, update app to not run a worker, pin numpy to v1

--- start_all.sh
+++ start_all.sh
@@ -3,19 +3,22 @@
 # Start the first process in the background
 uvicorn main:app --host 0.0.0.0 --port ${PRESTO_PORT} --reload &
 
+# Check if ROLE is set to "worker" and start workers if true
+if [ "$ROLE" = "worker" ]; then
+  NUM_WORKERS=${NUM_WORKERS:-1}  # Default to 1 worker if not specified
+
+  for i in $(seq 1 $NUM_WORKERS)
+  do
+    (
+      while true; do
+        echo "Starting run_worker.py instance $i..."
+        python run_worker.py worker-$i
+        echo "run_worker.py instance $i exited. Restarting..."
+        sleep 30  # Prevent potential rapid restart loop
+      done
+    ) &  # run workers as background processes
+  done
+fi
+
 # Start the second process in the foreground
-NUM_WORKERS=${NUM_WORKERS:-1}  # Default to 1 worker if not specified
-
-for i in $(seq 1 $NUM_WORKERS)
-do
-  (
-    while true; do
-      echo "Starting run_worker.py instance $i..."
-      python run_worker.py worker-$i
-      echo "run_worker.py instance $i exited. Restarting..."
-      sleep 30  # Prevent potential rapid restart loop
-    done
-  ) &.  # run workers as background processes
-done
-
 python run_processor.py
