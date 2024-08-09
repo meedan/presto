cc051a8e3229e53a5903e5e1ab319093715c0eb0
Devin Gaffney <itsme@devingaffney.com>
2024-08-09 07:13:11 -0700

    refactor to accommodate erroring in transformers

--- lib/model/generic_transformer.py
+++ lib/model/generic_transformer.py
@@ -1,9 +1,10 @@
 import os
-from typing import Union, Dict, List
+from typing import Union, List
 from sentence_transformers import SentenceTransformer
 from lib.logger import logger
 from lib.model.model import Model
 from lib import schemas
+from lib.cache import Cache
 
 class GenericTransformerModel(Model):
     def __init__(self, model_name: str):
@@ -17,20 +18,68 @@ class GenericTransformerModel(Model):
 
     def respond(self, docs: Union[List[schemas.Message], schemas.Message]) -> List[schemas.GenericItem]:
         """
-        Force messages as list of messages in case we get a singular item. Then, run fingerprint routine.
-        Respond can probably be genericized across all models.
+        Respond to a batch of messages by vectorizing uncached texts.
         """
-        if not isinstance(docs, list):
-            docs = [docs]
-        logger.info(docs)
-        vectorizable_texts = [e.body.text for e in docs]
-        vectorized = self.vectorize(vectorizable_texts)
-        for doc, vector in zip(docs, vectorized):
-            doc.body.result = vector
+        docs = self._ensure_list(docs)
+        self._log_docs(docs)
+
+        docs_to_process, texts_to_vectorize = self._separate_cached_docs(docs)
+
+        if texts_to_vectorize:
+            self._vectorize_and_cache(docs_to_process, texts_to_vectorize)
+
         return docs
+
+    def _ensure_list(self, docs: Union[List[schemas.Message], schemas.Message]) -> List[schemas.Message]:
+        """
+        Ensure the input is a list of messages.
+        """
+        return docs if isinstance(docs, list) else [docs]
+
+    def _log_docs(self, docs: List[schemas.Message]):
+        """
+        Log the documents for debugging purposes.
+        """
+        logger.info(docs)
+
+    def _separate_cached_docs(self, docs: List[schemas.Message]) -> (List[schemas.Message], List[str]):
+        """
+        Separate cached documents from those that need to be vectorized.
+        """
+        docs_to_process = []
+        texts_to_vectorize = []
+
+        for doc in docs:
+            cached_result = Cache.get_cached_result(doc.body.content_hash)
+            if cached_result:
+                doc.body.result = cached_result
+            else:
+                docs_to_process.append(doc)
+                texts_to_vectorize.append(doc.body.text)
+
+        return docs_to_process, texts_to_vectorize
+
+    def _vectorize_and_cache(self, docs_to_process: List[schemas.Message], texts_to_vectorize: List[str]):
+        """
+        Vectorize the uncached texts and store the results in the cache.
+        """
+        try:
+            vectorized = self.vectorize(texts_to_vectorize)
+            for doc, vector in zip(docs_to_process, vectorized):
+                doc.body.result = vector
+                Cache.set_cached_result(doc.body.content_hash, vector)
+        except Exception as e:
+            self.handle_fingerprinting_error(e)
 
     def vectorize(self, texts: List[str]) -> List[List[float]]:
         """
         Vectorize the text! Run as batch.
         """
         return self.model.encode(texts).tolist()
+
+    def handle_fingerprinting_error(self, error: Exception):
+        """
+        Handle any error that occurs during vectorization.
+        """
+        logger.error(f"Error during vectorization: {error}")
+        raise error
