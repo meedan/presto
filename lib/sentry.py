1559c88dbcdf62abbd267c8f9b99cb74a7868754
Devin Gaffney <itsme@devingaffney.com>
2025-01-03 06:09:39 -0800

    tweaks for bad env misfires

--- lib/sentry.py
+++ lib/sentry.py
@@ -1,15 +1,16 @@
 import os
 import sentry_sdk
 from lib.helpers import get_environment_setting
-
+DEPLOY_ENV = get_environment_setting("DEPLOY_ENV")
+SENTRY_DSN = get_environment_setting('sentry_sdk_dsn')
 sentry_sdk.init(
-    dsn=get_environment_setting('sentry_sdk_dsn'),
-    environment=get_environment_setting("DEPLOY_ENV"),
+    dsn=SENTRY_DSN,
+    environment=DEPLOY_ENV,
     traces_sample_rate=1.0,
 )
 
 def capture_custom_message(message, level='info', extra=None):
-    if get_environment_setting("DEPLOY_ENV") != 'local':
+    if DEPLOY_ENV and DEPLOY_ENV not in ['local', 'test']:
         with sentry_sdk.configure_scope() as scope:
             if extra:
                 for key, value in extra.items():
