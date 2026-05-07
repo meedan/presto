9cebc2e3c37b9ca078a0087a0a7c504d90690bb9
Sawy <melsawy.be7eri@gmail.com>
2026-05-07 17:51:12 +0300

    CV2-6699: fix tests

--- test/lib/queue/test_queue.py
+++ test/lib/queue/test_queue.py
@@ -29,8 +29,7 @@ class MockModelNoTimeout:
 class TestQueueWorker(unittest.TestCase):
     @patch('lib.queue.queue.boto3.resource')
     @patch('lib.helpers.get_environment_setting', return_value='us-west-1')
-    @patch('lib.telemetry.OpenTelemetryExporter.log_execution_time')
-    def setUp(self, mock_log_execution_time, mock_get_env_setting, mock_boto_resource):
+    def setUp(self, mock_get_env_setting, mock_boto_resource):
         self.model = AudioModel()
         self.model.model_name = "audio__Model"
         self.mock_model = MagicMock()
@@ -84,15 +83,11 @@ class TestQueueWorker(unittest.TestCase):
 
     @patch('lib.queue.worker.QueueWorker.log_and_handle_error')
     @patch('lib.queue.worker.time.time', side_effect=[0, 0.5])
-    @patch('lib.queue.worker.QueueWorker.log_execution_time')
-    @patch('lib.queue.worker.QueueWorker.log_execution_status')
-    def test_execute_with_timeout_success(self, mock_log_execution_status, mock_log_execution_time, mock_time, mock_log_error):
+    def test_execute_with_timeout_success(self, mock_time, mock_log_error):
         responses, success = self.queue.execute_with_timeout(MockModelNoTimeout(), [], timeout_seconds=1)
         self.assertEqual(responses in [[], ["response"]], True)
         self.assertTrue(success)
         mock_log_error.assert_not_called()
-        mock_log_execution_time.assert_called_once_with('timeout.MockModelNoTimeout', 0.5)
-        mock_log_execution_status.assert_called_once_with('timeout.MockModelNoTimeout', 'successful_message_response')
 
     def test_process(self):
         self.queue.receive_messages = MagicMock(return_value=[(FakeSQSMessage(receipt_handle="blah", body=json.dumps({
